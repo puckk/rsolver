3f5dfc55dfe3c962fdaeb204af3e78ba393d1698
Jeremías Pretto <jerepretto@gmail.com>
2019-07-04 01:24:24 -0300

    +

--- rsolver/scripts/common_prime.py
+++ rsolver/scripts/common_prime.py
@@ -1,7 +1,7 @@
 from secrets import randbits
 from sympy import nextprime, gcd
 from timeit import default_timer as timer
-
+import gmpy2
 
 def check(solver):
     if len(solver.datas["n"]) == 2 > 0:
@@ -14,11 +14,13 @@ def crack(solver):
     n2 = solver.datas["n"][1]
     e = solver.datas["e"][0]
     g = gcd(n1, n2)
+    print("!!!!!LPM",g)
     if g == 1:
         pass
     elif (n1/g) == int(n1/g):
-        p = g
-        q1 = n1/p
-        q2 = n2/p
-        solver.addpriv(int(p), int(q1), e, n1)
-        solver.addpriv(int(p), int(q2), e, n2)
+        if gmpy2.is_prime(int(g)):
+            p = g
+            q1 = n1/p
+            q2 = n2/p
+            solver.addpriv(int(p), int(q1), e, n1)
+            solver.addpriv(int(p), int(q2), e, n2)
