22646b8ac95285981e21d2e854e466f919fa52b9
Jeremias Pretto <jerepretto@gmail.com>
2019-12-09 22:29:19 -0300

    low q

--- rsolver/scripts/lowq.py
+++ rsolver/scripts/lowq.py
@@ -8,18 +8,17 @@ def rwh_primes1(n):
             return [2] + [2*i+1 for i in range(1,n//2) if sieve[i]]
 
 def check(solver):
-    return False
     if (len(solver.datas["n"])>0):
         return True
     return False
 
 def crack (solver):
     n=solver.datas["n"][-1]
-    for prime in (rwh_primes1(100000)):
+    for prime in (rwh_primes1(57970028)):			      
         if n % prime == 0:
             p=prime
             q=n//prime
-    solver.addq(q)
-    solver.addp(p)
+            solver.addq(q)
+            solver.addp(p)
 
     #Ahora se queda con el último, podría quedarse con todos y bruteforcear las keys y desencripts
